f2072d1909e4bce954873d11267a84b4fbcf624d
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-09-14 13:30:14 -0600

    cambios

---
+++ iAcademicGenerator.BusinessLogic/ServiceResultType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iAcademicGenerator.BusinessLogic
+{
+    public enum ServiceResultType
+    {
+        Success = 200,
+        Created = 201,
+        BadRequest = 400,
+        NotFound = 404,
+        Conflict = 409,
+        InternalServerError = 500,
+        DatabaseError = 501,
+        ValidationError = 422
+    }
+}
